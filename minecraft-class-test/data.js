ca32c0cd49b65fe30f8911b0c844ed4a5078f88c
gus <67655055+Kittyninja@users.noreply.github.com>
2021-01-27 08:40:22 -0600

    Create data.js

---
+++ minecraft-class-test/data.js
@@ -0,0 +1,76 @@
+var questions = [{
+        name: "What is your favorite tool?",
+        answers: [{
+         name : "Axe",
+         feeder : "Hunter"
+        },
+        {
+         name : "Sword",
+         feeder : "Warrior"
+        },
+        {
+         name : "Pickaxe",
+         feeder : "Miner"
+        },
+        {
+         name : "Hoe",
+         feeder : "Brewer"
+        },
+        {
+         name : "Shovel",
+         feeder : "Builder"
+        }
+        ],
+        user: null
+      },
+                     {
+        name: "What is your favorite block?",
+        answers: [{
+         name : "Cobblestone",
+         feeder : "Hunter"
+        },
+        {
+         name : "Diamond Block",
+         feeder : "Warrior"
+        },
+        {
+         name : "Gold Ore",
+         feeder : "Miner"
+        },
+        {
+         name : "Obsidian",
+         feeder : "Brewer"
+        },
+        {
+         name : "Wooden Plank",
+         feeder : "Builder"
+        },
+        ],
+        user: null
+      },
+        {
+        name: "What is your favorite minigame?",
+        answers: [{
+         name : "Bedwars",
+         feeder : "Hunter"
+        },
+        {
+         name : "Skywars",
+         feeder : "Warrior"
+        },
+        {
+         name : "Block Hunt",
+         feeder : "Miner"
+        },
+        {
+         name : "Item Collect",
+         feeder : "Brewer"
+        },
+        {
+         name : "Build Battle",
+         feeder : "Builder"
+        },
+        ],
+        user: null
+      }
+    ]
